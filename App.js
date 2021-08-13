2062d1f0b13b61a3fa1c090a60b8a42189905d8c
Ansruta <75419631+Ansruta@users.noreply.github.com>
2021-08-13 19:45:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs' 
